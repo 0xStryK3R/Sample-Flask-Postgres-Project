321a1bb3940923df0c726eebd2f8b9256c7e6daf
Arup Bhowmcik <arupbhowmick007@gmail.com>
2022-08-11 19:59:59 +0530

    Sample Flask Postgres Project

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1> Creating a Sample Flask Project Using Postgres DB <h1>
+
+Copy and replace sample_config.py to config.py, and replace with your data
---
+++ sample_config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 19:01:55 2022
+
+@author: arupb
+"""
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/flasksql'
+SECRET_KEY = 'test'
